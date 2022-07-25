cb0fa307e2461553ae1c32340a036b299f854507
Marc Duiker <marcduiker@gmail.com>
2022-07-25 23:04:04 +0200

    Add wave function collapse demo

---
+++ 2D/wave-function-collapse/sketch.js
@@ -0,0 +1,209 @@
+// Wave Function Collapse (tiled model)
+// The Coding Train / Daniel Shiffman
+// https://thecodingtrain.com/challenges/171-wave-function-collapse
+// https://youtu.be/0zac-cDzJwA
+
+// Code from Challenge: https://editor.p5js.org/codingtrain/sketches/pLW3_PNDM
+// Corrected and Expanded: https://github.com/CodingTrain/Wave-Function-Collapse
+
+// Array for tiles and tile images
+let tiles = [];
+const tileImages = [];
+const nrOfImages = 8;
+
+// Current state of the grid
+let grid = [];
+
+// Width and height of each cell
+const DIM = 25;
+
+// Load images
+function preload() {
+  const path = "tiles";
+  for (let i = 0; i < nrOfImages; i++) {
+    tileImages[i] = loadImage(`${path}/${i}.png`);
+  }
+}
+
+function removeDuplicatedTiles(tiles) {
+  const uniqueTilesMap = {};
+  for (const tile of tiles) {
+    const key = tile.edges.join(','); // ex: "ABB,BCB,BBA,AAA"
+    uniqueTilesMap[key] = tile;
+  }
+  return Object.values(uniqueTilesMap);
+}
+
+
+function setup() {
+  createCanvas(800, 800);
+
+  // Create and label the tiles
+  tiles[0] = new Tile(tileImages[0], ["A", "A", "A", "A"]);
+  tiles[1] = new Tile(tileImages[1], ["B", "A", "B", "A"]);
+  tiles[2] = new Tile(tileImages[2], ["B", "B", "B", "B"]);
+  tiles[3] = new Tile(tileImages[3], ["A", "B", "A", "B"]);
+  tiles[4] = new Tile(tileImages[4], ["B", "B", "A", "A"]);
+  tiles[5] = new Tile(tileImages[5], ["B", "C", "B", "A"]);
+  tiles[6] = new Tile(tileImages[6], ["B", "DE", "ED", "A"]);
+  tiles[7] = new Tile(tileImages[7], ["DE", "F", "ED", "A"]);
+
+  for (let i = 0; i < nrOfImages; i++) {
+    tiles[i].index = i;
+  }
+
+  const initialTileCount = tiles.length;
+  for (let i = 0; i < initialTileCount; i++) {
+    let tempTiles = [];
+    for (let j = 0; j < 4; j++) {
+      tempTiles.push(tiles[i].rotate(j));
+    }
+    tempTiles = removeDuplicatedTiles(tempTiles);
+    tiles = tiles.concat(tempTiles);
+  }
+  console.log(tiles.length);
+
+  // Generate the adjacency rules based on edges
+  for (let i = 0; i < tiles.length; i++) {
+    const tile = tiles[i];
+    tile.analyze(tiles);
+  }
+
+  startOver();
+}
+
+function startOver() {
+  // Create cell for each spot on the grid
+  for (let i = 0; i < DIM * DIM; i++) {
+    grid[i] = new Cell(tiles.length);
+  }
+}
+
+function checkValid(arr, valid) {
+  //console.log(arr, valid);
+  for (let i = arr.length - 1; i >= 0; i--) {
+    // VALID: [BLANK, RIGHT]
+    // ARR: [BLANK, UP, RIGHT, DOWN, LEFT]
+    // result in removing UP, DOWN, LEFT
+    let element = arr[i];
+    // console.log(element, valid.includes(element));
+    if (!valid.includes(element)) {
+      arr.splice(i, 1);
+    }
+  }
+  // console.log(arr);
+  // console.log("----------");
+}
+
+function mousePressed() {
+  redraw();
+}
+
+function draw() {
+  background(0);
+
+  const w = width / DIM;
+  const h = height / DIM;
+  for (let j = 0; j < DIM; j++) {
+    for (let i = 0; i < DIM; i++) {
+      let cell = grid[i + j * DIM];
+      if (cell.collapsed) {
+        let index = cell.options[0];
+        image(tiles[index].img, i * w, j * h, w, h);
+      } else {
+        noFill();
+        stroke(51);
+        rect(i * w, j * h, w, h);
+      }
+    }
+  }
+
+  // Pick cell with least entropy
+  let gridCopy = grid.slice();
+  gridCopy = gridCopy.filter((a) => !a.collapsed);
+  // console.table(grid);
+  // console.table(gridCopy);
+
+  if (gridCopy.length == 0) {
+    return;
+  }
+  gridCopy.sort((a, b) => {
+    return a.options.length - b.options.length;
+  });
+
+  let len = gridCopy[0].options.length;
+  let stopIndex = 0;
+  for (let i = 1; i < gridCopy.length; i++) {
+    if (gridCopy[i].options.length > len) {
+      stopIndex = i;
+      break;
+    }
+  }
+
+  if (stopIndex > 0) gridCopy.splice(stopIndex);
+  const cell = random(gridCopy);
+  cell.collapsed = true;
+  const pick = random(cell.options);
+  if (pick === undefined) {
+    startOver();
+    return;
+  }
+  cell.options = [pick];
+
+  const nextGrid = [];
+  for (let j = 0; j < DIM; j++) {
+    for (let i = 0; i < DIM; i++) {
+      let index = i + j * DIM;
+      if (grid[index].collapsed) {
+        nextGrid[index] = grid[index];
+      } else {
+        let options = new Array(tiles.length).fill(0).map((x, i) => i);
+        // Look up
+        if (j > 0) {
+          let up = grid[i + (j - 1) * DIM];
+          let validOptions = [];
+          for (let option of up.options) {
+            let valid = tiles[option].down;
+            validOptions = validOptions.concat(valid);
+          }
+          checkValid(options, validOptions);
+        }
+        // Look right
+        if (i < DIM - 1) {
+          let right = grid[i + 1 + j * DIM];
+          let validOptions = [];
+          for (let option of right.options) {
+            let valid = tiles[option].left;
+            validOptions = validOptions.concat(valid);
+          }
+          checkValid(options, validOptions);
+        }
+        // Look down
+        if (j < DIM - 1) {
+          let down = grid[i + (j + 1) * DIM];
+          let validOptions = [];
+          for (let option of down.options) {
+            let valid = tiles[option].up;
+            validOptions = validOptions.concat(valid);
+          }
+          checkValid(options, validOptions);
+        }
+        // Look left
+        if (i > 0) {
+          let left = grid[i - 1 + j * DIM];
+          let validOptions = [];
+          for (let option of left.options) {
+            let valid = tiles[option].right;
+            validOptions = validOptions.concat(valid);
+          }
+          checkValid(options, validOptions);
+        }
+
+        // I could immediately collapse if only one option left?
+        nextGrid[index] = new Cell(options);
+      }
+    }
+  }
+
+  grid = nextGrid;
+}
